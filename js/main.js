e2c25af51e3408bd56e4bb8ab302b41ca0417de5
Fidelis E. Peter <fidelisepeter#gmail.com>
2024-01-18 17:52:42 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,82 +1,174 @@
-$(function () {
-  var siteSticky = function () {
-    $(".js-sticky-header").sticky({ topSpacing: 0 });
-  };
-  siteSticky();
+(function () {
+  "use strict";
 
-  var siteMenuClone = function () {
-    $(".js-clone-nav").each(function () {
-      var $this = $(this);
-      $this
-        .clone()
-        .attr("class", "site-nav-wrap")
-        .appendTo(".site-mobile-menu-body");
-    });
+  AOS.init({
+    ease: "slide",
+    once: true,
+  });
 
-    setTimeout(function () {
-      var counter = 0;
-      $(".site-mobile-menu .has-children").each(function () {
-        var $this = $(this);
-
-        $this.prepend('<span class="arrow-collapse collapsed">');
-
-        $this.find(".arrow-collapse").attr({
-          "data-toggle": "collapse",
-          "data-target": "#collapseItem" + counter,
-        });
-
-        $this.find("> ul").attr({
-          class: "collapse",
-          id: "collapseItem" + counter,
-        });
-
-        counter++;
+  var slider = function () {
+    var carouselEvents = document.querySelectorAll(".carousel-events");
+    if (carouselEvents.length > 0) {
+      var eventsSlider = tns({
+        container: ".carousel-events",
+        items: 1,
+        mode: "carousel",
+        autoplay: true,
+        animateIn: "tns-fadeIn",
+        animateOut: "tns-fadeOut",
+        speed: 700,
+        nav: true,
+        gutter: 20,
+        controls: false,
+        autoHeight: true,
+        autoplayButtonOutput: false,
+        responsive: {
+          0: {
+            items: 1,
+            gutter: 0,
+          },
+          600: {
+            items: 2,
+            gutter: 20,
+          },
+          1000: {
+            items: 3,
+            gutter: 20,
+          },
+        },
       });
-    }, 1000);
+    }
 
-    $("body").on("click", ".arrow-collapse", function (e) {
-      var $this = $(this);
-      if ($this.closest("li").find(".collapse").hasClass("show")) {
-        $this.removeClass("active");
-      } else {
-        $this.addClass("active");
-      }
-      e.preventDefault();
-    });
-
-    $(window).resize(function () {
-      var $this = $(this),
-        w = $this.width();
-
-      if (w > 768) {
-        if ($("body").hasClass("offcanvas-menu")) {
-          $("body").removeClass("offcanvas-menu");
-        }
-      }
-    });
-
-    $("body").on("click", ".js-menu-toggle", function (e) {
-      var $this = $(this);
-      e.preventDefault();
-
-      if ($("body").hasClass("offcanvas-menu")) {
-        $("body").removeClass("offcanvas-menu");
-        $this.removeClass("active");
-      } else {
-        $("body").addClass("offcanvas-menu");
-        $this.addClass("active");
-      }
-    });
-
-    // click outisde offcanvas
-    $(document).mouseup(function (e) {
-      var container = $(".site-mobile-menu");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($("body").hasClass("offcanvas-menu")) {
-          $("body").removeClass("offcanvas-menu");
-        }
-      }
-    });
+    var carouselSlider = document.querySelectorAll(".carousel-testimony");
+    if (carouselSlider.length > 0) {
+      var testimonySlider = tns({
+        container: ".carousel-testimony",
+        items: 1,
+        mode: "carousel",
+        autoplay: true,
+        animateIn: "tns-fadeIn",
+        animateOut: "tns-fadeOut",
+        speed: 700,
+        nav: true,
+        gutter: 20,
+        controls: false,
+        autoplayButtonOutput: false,
+        responsive: {
+          0: {
+            items: 1,
+            gutter: 0,
+          },
+          600: {
+            items: 2,
+            gutter: 20,
+          },
+          1000: {
+            items: 3,
+            gutter: 20,
+          },
+        },
+      });
+    }
   };
-  siteMenuClone();
-});
+  slider();
+
+  var swiper = new Swiper(".swiper-container", {
+    // effect: "cards",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+  });
+
+  //COUNTER
+  ("use trict");
+  // How long you want the animation to take, in ms
+  const animationDuration = 2000;
+  // Calculate how long each ‘frame’ should last if we want to update the animation 60 times per second
+  const frameDuration = 1000 / 60;
+  // Use that to calculate how many frames we need to complete the animation
+  const totalFrames = Math.round(animationDuration / frameDuration);
+  // An ease-out function that slows the count as it progresses
+  const easeOutQuad = (t) => t * (2 - t);
+
+  const numberWithCommas = (n) => {
+    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  // The animation function, which takes an Element
+  const animateCountUp = (el) => {
+    let frame = 0;
+    const countTo = parseInt(el.innerHTML, 10);
+    // Start the animation running 60 times per second
+    const counter = setInterval(() => {
+      frame++;
+      // Calculate our progress as a value between 0 and 1
+      // Pass that value to our easing function to get our
+      // progress on a curve
+      const progress = easeOutQuad(frame / totalFrames);
+      // Use the progress value to calculate the current count
+      const currentCount = Math.round(countTo * progress);
+
+      // If the current count has changed, update the element
+      if (parseInt(el.innerHTML, 10) !== currentCount) {
+        el.innerHTML = numberWithCommas(currentCount);
+      }
+
+      // If we’ve reached our last frame, stop the animation
+      if (frame === totalFrames) {
+        clearInterval(counter);
+      }
+    }, frameDuration);
+  };
+
+  // Run the animation on all elements with a class of ‘countup’
+  const runAnimations = () => {
+    const countupEls = document.querySelectorAll(".countup");
+    countupEls.forEach(animateCountUp);
+  };
+
+  // In Viewed
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll(".section-counter");
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    var i;
+    for (i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        if (!element.classList.contains("viewed")) {
+          element.classList.add("viewed");
+          runAnimations();
+        } else {
+          if (element.classList.contains("viewed")) {
+          }
+        }
+      }
+    }
+  }
+
+  window.addEventListener("scroll", checkPosition);
+  window.addEventListener("resize", init);
+
+  init();
+  checkPosition();
+
+  //GLIGHTBOX
+  const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true,
+  });
+})();
